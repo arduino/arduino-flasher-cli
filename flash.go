f55a36ce774fcaee867f7e1836ce8fcd7269d5d7
MatteoPologruto <109663225+MatteoPologruto@users.noreply.github.com>
2025-09-17 11:40:43 +0200

    Add arduino-flasher-cli tool

---
+++ flash.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"arduino-flasher/updater"
+	"context"
+	"fmt"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/arduino/go-paths-helper"
+	"github.com/bcmi-labs/orchestrator/cmd/feedback"
+	"github.com/bcmi-labs/orchestrator/cmd/i18n"
+	"github.com/spf13/cobra"
+)
+
+func newFlashCmd() *cobra.Command {
+	var forceYes bool
+	appCmd := &cobra.Command{
+		Use:   "flash",
+		Short: "Flash a Debian image on the board",
+		Long:  "Dowload the specified Debian image version and flash it on the board",
+		Example: " " + os.Args[0] + " flash latest\n" +
+			" " + os.Args[0] + " flash 20250915-173\n",
+		Args: cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			runFlashCommand(cmd.Context(), args, forceYes)
+		},
+	}
+	appCmd.Flags().BoolVarP(&forceYes, "yes", "y", false, "Automatically confirm all prompts")
+	// TODO: add --clean-install flag or something similar to distinguish between keeping and purging the /home directory
+
+	return appCmd
+}
+
+func runFlashCommand(ctx context.Context, args []string, forceYes bool) {
+	version := args[0]
+
+	updateURL := os.Getenv("UPDATE_URL")
+	if updateURL == "" {
+		// TODO: change to prod
+		updateURL = "https://downloads.arduino.cc"
+	}
+
+	parsedURL, err := url.Parse(updateURL)
+	if err != nil {
+		feedback.Fatal(i18n.Tr("invalid UPDATE_URL:", err), feedback.ErrBadArgument)
+	}
+
+	headers := map[string]string{}
+	clientID := os.Getenv("CF_ACCESS_CLIENT_ID")
+	clientSecret := os.Getenv("CF_ACCESS_CLIENT_SECRET")
+	if clientID != "" && clientSecret != "" {
+		headers["CF-Access-Client-Id"] = clientID
+		headers["CF-Access-Client-Secret"] = clientSecret
+	}
+
+	var client *updater.Client
+	if len(headers) == 2 {
+		client = updater.NewClient(parsedURL, "debian-im/Stable", updater.WithHeaders(headers))
+	} else {
+		client = updater.NewClient(parsedURL, "debian-im/Stable")
+	}
+
+	downloadedImagePath, updatedVersion, err := updater.DownloadImage(client, version, func(target string) (bool, error) {
+		feedback.Printf("Found Debian image version: %s", target)
+		feedback.Printf("Do you want to download it and flash it on the board? (yes/no)")
+
+		var yesInput string
+		_, err := fmt.Scanf("%s\n", &yesInput)
+		if err != nil {
+			return false, err
+		}
+		yes := strings.ToLower(yesInput) == "yes" || strings.ToLower(yesInput) == "y"
+		return yes, nil
+	}, forceYes)
+
+	if err != nil {
+		feedback.Fatal(i18n.Tr("error downloading the image: %v", err), feedback.ErrBadArgument)
+	}
+
+	if downloadedImagePath != "" {
+		defer paths.New(downloadedImagePath).RemoveAll()
+
+		if err := updater.FlashBoard(ctx, downloadedImagePath, updatedVersion); err != nil {
+			feedback.Fatal(i18n.Tr("error flashing the board: %v", err), feedback.ErrBadArgument)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,22 @@
+module arduino-flasher
+
+go 1.25
+
+require (
+	github.com/arduino/go-paths-helper v1.14.0
+	github.com/bcmi-labs/orchestrator v0.3.0
+	github.com/codeclysm/extract/v4 v4.0.0
+	github.com/spf13/cobra v1.9.1
+	go.bug.st/cleanup v1.0.0
+)
+
+require (
+	github.com/h2non/filetype v1.1.3 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/juju/errors v1.0.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/pflag v1.0.7 // indirect
+	github.com/ulikunitz/xz v0.5.12 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+)
