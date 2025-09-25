cd6fe70d10de6d877406f9b76ffce78057e72f22
MatteoPologruto <109663225+MatteoPologruto@users.noreply.github.com>
2025-09-25 09:01:52 -0700

    Flasher: fix inconsistent checksum and delete temporary directories at each run

--- flash.go
+++ flash.go
@@ -3,11 +3,8 @@ package main
 import (
 	"arduino-flasher/updater"
 	"context"
-	"fmt"
-	"net/url"
 	"os"
 	"runtime"
-	"strings"
 
 	"github.com/arduino/go-paths-helper"
 	runas "github.com/arduino/go-windows-runas"
@@ -53,76 +50,8 @@ func runFlashCommand(ctx context.Context, args []string, forceYes bool) {
 		feedback.Fatal(i18n.Tr("could not find image absolute path: %v", err), feedback.ErrBadArgument)
 	}
 
-	if !imagePath.Exist() {
-		version := args[0]
-
-		updateURL := os.Getenv("UPDATE_URL")
-		if updateURL == "" {
-			// TODO: change to prod
-			updateURL = "https://downloads.oniudra.cc"
-		}
-
-		parsedURL, err := url.Parse(updateURL)
-		if err != nil {
-			feedback.Fatal(i18n.Tr("invalid UPDATE_URL:", err), feedback.ErrBadArgument)
-		}
-
-		headers := map[string]string{}
-		clientID := os.Getenv("CF_ACCESS_CLIENT_ID")
-		clientSecret := os.Getenv("CF_ACCESS_CLIENT_SECRET")
-		if clientID != "" && clientSecret != "" {
-			headers["CF-Access-Client-Id"] = clientID
-			headers["CF-Access-Client-Secret"] = clientSecret
-		}
-
-		var client *updater.Client
-		if len(headers) == 2 {
-			client = updater.NewClient(parsedURL, "debian-im/Stable", updater.WithHeaders(headers))
-		} else {
-			client = updater.NewClient(parsedURL, "debian-im/Stable")
-		}
-
-		tempImagePath, err := updater.DownloadAndExtract(client, version, func(target string) (bool, error) {
-			feedback.Printf("Found Debian image version: %s", target)
-			feedback.Printf("Do you want to download it and flash it on the board? (yes/no)")
-
-			var yesInput string
-			_, err := fmt.Scanf("%s\n", &yesInput)
-			if err != nil {
-				return false, err
-			}
-			yes := strings.ToLower(yesInput) == "yes" || strings.ToLower(yesInput) == "y"
-			return yes, nil
-		}, forceYes)
-
-		if err != nil {
-			feedback.Fatal(i18n.Tr("could not download and extract the image: %v", err), feedback.ErrBadArgument)
-		}
-
-		defer tempImagePath.Parent().RemoveAll()
-
-		imagePath = tempImagePath
-	} else if !imagePath.IsDir() {
-		temp, err := paths.MkTempDir("", "debian-image-")
-		if err != nil {
-			feedback.Fatal(i18n.Tr("error creating a temporary directory to extract the archive: %v", err), feedback.ErrBadArgument)
-		}
-		defer temp.RemoveAll()
-
-		err = updater.ExtractImage(imagePath, temp)
-		if err != nil {
-			feedback.Fatal(i18n.Tr("error extracting the archive: %v", err), feedback.ErrBadArgument)
-		}
-
-		tempContent, err := temp.ReadDir(paths.AndFilter(paths.FilterDirectories(), paths.FilterPrefixes("arduino-unoq-debian-image-")))
-		if err != nil {
-			feedback.Fatal(i18n.Tr("could not find Debian image directory: %v", err), feedback.ErrBadArgument)
-		}
-
-		imagePath = tempContent[0]
-	}
-
-	if err := updater.FlashBoard(ctx, imagePath.String()); err != nil {
+	err = updater.Flash(ctx, imagePath, args[0], forceYes)
+	if err != nil {
 		feedback.Fatal(i18n.Tr("error flashing the board: %v", err), feedback.ErrBadArgument)
 	}
 }
