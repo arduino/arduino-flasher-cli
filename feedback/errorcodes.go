e89760a09b2e3cb8a8390cfd7843a5844b57419d
dido <davideneri18@gmail.com>
2025-10-15 11:49:29 +0200

    feat: update copyright and licensing information in source files

--- feedback/errorcodes.go
+++ feedback/errorcodes.go
@@ -1,9 +1,9 @@
-// This file is part of arduino-cli.
+// This file is part of arduino-flasher-cli.
 //
-// Copyright 2022 ARDUINO SA (http://www.arduino.cc/)
+// Copyright 2025 ARDUINO SA (http://www.arduino.cc/)
 //
 // This software is released under the GNU General Public License version 3,
-// which covers the main part of arduino-cli.
+// which covers the main part of arduino-flasher-cli.
 // The terms of this license can be found at:
 // https://www.gnu.org/licenses/gpl-3.0.en.html
 //
