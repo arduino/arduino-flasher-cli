e1ea0d3654f02ff172d6d0366c42a062f031b145
MatteoPologruto <m.pologruto@arduino.cc>
2025-12-18 12:49:13 +0100

    Sort releases in descending build_id order

--- service/service_list.go
+++ service/service_list.go
@@ -16,10 +16,13 @@
 package service
 
 import (
+	"cmp"
 	"context"
+	"slices"
 
 	"github.com/arduino/arduino-flasher-cli/internal/updater"
 	flasher "github.com/arduino/arduino-flasher-cli/rpc/cc/arduino/flasher/v1"
+	"go.bug.st/f"
 )
 
 func (s *flasherServerImpl) List(ctx context.Context, req *flasher.ListRequest) (*flasher.ListResponse, error) {
@@ -30,13 +33,23 @@ func (s *flasherServerImpl) List(ctx context.Context, req *flasher.ListRequest) 
 		return nil, err
 	}
 
-	resp := &flasher.ListResponse{}
-	for i := len(manifest.Releases) - 1; i >= 0; i-- {
-		resp.Releases = append(resp.Releases, &flasher.Release{
-			BuildId: manifest.Releases[i].Version,
-			Latest:  manifest.Releases[i].Version == manifest.Latest.Version,
-		})
-	}
+	releases := f.Map(manifest.Releases, func(r updater.Release) *flasher.Release {
+		return &flasher.Release{
+			BuildId: r.Version,
+			Latest:  r.Version == manifest.Latest.Version,
+		}
+	})
+	slices.SortFunc(releases, func(a, b *flasher.Release) int {
+		if a.Latest {
+			if b.Latest {
+				return 0
+			}
+			return -1
+		} else if b.Latest {
+			return +1
+		}
+		return cmp.Compare(b.BuildId, a.BuildId)
+	})
 
-	return resp, nil
+	return &flasher.ListResponse{Releases: releases}, nil
 }
