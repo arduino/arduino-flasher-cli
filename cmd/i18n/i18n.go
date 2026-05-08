53425d39179bac0838c482a04132e19c46950af4
dido18 <davideneri18@gmail.com>
2026-05-08 15:57:13 +0200

    addind reuse on go files

--- cmd/i18n/i18n.go
+++ cmd/i18n/i18n.go
@@ -1,3 +1,8 @@
+// This file is part of arduino-flasher-cli.
+//
+// SPDX-FileCopyrightText: Arduino s.r.l. and/or its affiliated companies
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 package i18n
 
 import "fmt"
