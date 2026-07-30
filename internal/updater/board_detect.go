07d4c1e61525c1cf62174501a3ad793f43e78646
MatteoPologruto <m.pologruto@arduino.cc>
2026-07-30 15:26:50 +0200

    feat: support flashing of specific ubuntu images versions

--- internal/updater/board_detect.go
+++ internal/updater/board_detect.go
@@ -8,6 +8,7 @@ package updater
 import (
 	"context"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"strings"
 
@@ -24,20 +25,31 @@ const latest = "latest"
 func DetectBoardAndSetOs(ctx context.Context, version string) (string, string, string, error) {
 	boardType := registry.UnoQ
 	os := registry.Debian
-	switch version {
-	case latest:
+	switch {
+	case version == latest:
 		var err error
 		feedback.Print(i18n.Tr("Detecting board type. Please connect the board in EDL mode."))
 		boardType, err = DetectBoardType(ctx)
 		if err != nil {
 			return "", "", "", err
 		}
-	case "ventunoq":
+	case version == "ventunoq":
 		version = latest
 		boardType = registry.VentunoQ
 		os = registry.Ubuntu
-	case "unoq":
+	case version == "unoq":
 		version = latest
+	case strings.HasPrefix(version, "ubuntu-"):
+		version = strings.TrimPrefix(version, "ubuntu-")
+		boardType = registry.VentunoQ
+		os = registry.Ubuntu
+	case strings.HasPrefix(version, "debian-"):
+		version = strings.TrimPrefix(version, "debian-")
+		// For now we assume that the board is an UNO Q, but in the future we should detect the board type.
+		boardType = registry.UnoQ
+		os = registry.Debian
+	default:
+		return "", "", "", errors.New(i18n.Tr("invalid version: %s", version))
 	}
 	return version, boardType, os, nil
 }
